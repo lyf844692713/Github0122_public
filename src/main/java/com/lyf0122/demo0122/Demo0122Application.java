cc3f16d1f450e23d5dde86d092da500ea0c147e5
“lyf844692713” <844692713@qq.com>
2019-01-22 16:25:38 +0800

    1

---
+++ src/main/java/com/lyf0122/demo0122/Demo0122Application.java
@@ -0,0 +1,21 @@
+package com.lyf0122.demo0122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Demo0122Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo0122Application.class, args);
+	}
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello";
+	}
+
+}
+
